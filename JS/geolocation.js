14123b41fde7be14bd80d4f6f941b44d9424b17f
Mika L <mikalintula@kamk.fi>
2019-06-16 15:08:28 +0300

    Everything got stripped to bare minimum so it is easy to start to work on it

--- JS/geolocation.js
+++
@@ -1,7 +0,0 @@
-function myMap() {
-	var home = {lat: 64.23, lng: 27.73};
-	var mapProp= {center: home, zoom:7};
-	var map=new google.maps.Map(document.getElementById('googleMap'),mapProp);
-	var marker = new google.maps.Marker({ position: home, map: map});
-}
-
